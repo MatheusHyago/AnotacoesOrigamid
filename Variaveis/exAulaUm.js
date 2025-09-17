4a327b9fa555710a3cfe57682fb4bb3fae2ea99d
litch <matheushyago92@gmail.com>
2025-09-16 23:10:22 -0300

    Ex 1 aula variaveis

---
+++ Variaveis/exAulaUm.js
@@ -0,0 +1,22 @@
+// declarar uma variável com seu nome
+var nome = "koksuhibo";
+
+//declarar uma variável com sua idade
+var idade = 20;
+
+//declarar uma variável com sua comida favorita e não atribuir valor
+var comidaFavorita;
+
+//atribuir valor a sua comida favorita
+comidaFavorita = "Salada de brocolis";
+
+//declarar 5 variáveis diferentes sem valor
+
+var cidade,
+    time,
+    país,
+    corFavorita,
+    animalDeEstimacao;
+
+//imprimir na tela (console) as variáveis que você acabou de criar
+console.log(nome, idade, comidaFavorita, cidade);
