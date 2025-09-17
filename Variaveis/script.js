9f08df5d5c696ed0ae03ac5d82dacd1a647d9a16
litch <matheushyago92@gmail.com>
2025-09-16 23:03:46 -0300

    aula 1

---
+++ Variaveis/script.js
@@ -0,0 +1,59 @@
+// linguagem interpretada
+
+//variavel É case sensitive
+//console.log(nome); // correto
+//pode ser sem valor ex unfeined
+
+//variaveis pode comcar com $ ou _
+//variaveis não podem começar com números
+//variaveis não podem ter espaços ou traços
+
+//jquery com $
+//ex var $selecionar = document.querySelector("h1");
+//ex var _nome = "Muzé";
+
+//let nao deixa declarar a mesma variavel
+//const é uma variavel que não pode ser alterada
+
+console.log("Koksuhibo$$!");
+
+//variaveis var, let, const
+//variaveis servem pra evitar repetição de código
+var nome = "Muzé";
+console.log(nome);
+
+//variaveis podem ser declaradas sem valor
+var Undefined;
+console.log(Undefined);
+
+var totalCompras = 10;
+var totalPreço = 20;
+var totalPagar = totalCompras * totalPreço;
+console.log(totalPagar);
+
+// ex de var com mais de uma palavra definida 
+//pode ser declarado mais de uma variável na mesma linha
+var nomeCompleto = "Muzé Silva", 
+    idade = 30, 
+    racaa = "Oni"; 
+console.log(nomeCompleto, idade, racaa);
+
+//hosting
+
+var cidade;
+console.log(cidade); // undefined
+var cidade = "São Paulo";
+console.log(cidade); // São Paulo
+
+let time = "Corinthians";
+console.log(time);
+//ex erro
+;// let time = "Palmeiras"; // erro
+
+const anime = "Shingeki no Kyojin";
+console.log(anime);
+
+//ex erro
+//  anime = "Naruto";
+
+
