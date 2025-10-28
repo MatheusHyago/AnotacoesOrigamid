2595e425b1136ac0168414db63ae0c536518a294
litch <matheushyago92@gmail.com>
2025-10-28 20:24:20 -0300

    w

---
+++ DOM/script.js
@@ -0,0 +1,20 @@
+//DOM Document Object Model (DOM)
+//É uma interface que representa documentos HTML e XML através de objetos. Com ela é possível manipular a estrutura, estilo e conteúdo destes documentos.
+
+
+//Window objeto maximo que existe seguido pelo document
+
+console.log(window);
+//retorna um objeto sempre que existe f na frente e um metodo
+
+window.alert('Teste Window on DOM');
+//se a propriedade retorna um objeto posso acerssar todos os metodos f dela
+//ex
+
+//const h1selecionado = document.querySelector('h1');
+//const h1selecionado = document.querySelector('h1');
+//h1selecionado 
+//<h1>​TESTE DOM QUERRY SELECTOR​</h1>​
+
+const href = window.location.href;
+console.log(href);
